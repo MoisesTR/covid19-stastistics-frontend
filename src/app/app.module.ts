56ded646b57bc75cf9513d37b942a8e7c2778935
mtrigueros <moisestrigueros@hotmail.com>
2021-02-02 23:24:31 -0600

    SC-9 Import main modules in app module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,15 +2,24 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { MDBBootstrapModulesPro, MDBSpinningPreloader, ToastModule } from 'ng-uikit-pro-standard';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [AppComponent],
   imports: [
-    BrowserModule
+    BrowserModule,
+    MDBBootstrapModulesPro.forRoot(),
+    ToastModule.forRoot({
+      opacity: 1,
+      preventDuplicates: false,
+      maxOpened: 3,
+      autoDismiss: true,
+      timeOut: 5000,
+    }),
+    BrowserAnimationsModule,
   ],
-  providers: [],
-  bootstrap: [AppComponent]
+  providers: [MDBSpinningPreloader],
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
